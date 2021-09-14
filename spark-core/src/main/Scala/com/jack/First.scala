afff74da9f8692dc5113073ed90ae93d1a5e29e4
dengjirui <1298573296@qq.com>
2021-09-14 13:56:49 +0800

    删除idea

---
+++ spark-core/src/main/Scala/com/jack/First.scala
@@ -0,0 +1,13 @@
+package com.jack
+
+/**
+ * @author jack Deng
+ * @date 2021/9/14 13:55
+ * @version 1.0
+ */
+object First {
+  def main(args: Array[String]): Unit = {
+    print("hello,spark")
+  }
+
+}
