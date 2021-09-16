44aa2efd6be3066bb61a3525a3a8fc8698224e9b
dengjirui <1298573296@qq.com>
2021-09-16 10:19:40 +0800

    添加文件

--- spark-core/src/main/Scala/com/jack/partition/rddMemoryPartition.scala
+++ spark-core/src/main/Scala/com/jack/partition/rddMemoryPartition.scala
@@ -5,23 +5,24 @@ import org.apache.spark.{SparkConf, SparkContext}
 
 object rddMemoryPartition {
   def main(args: Array[String]): Unit = {
-    val conf = new SparkConf().setMaster("local[*]").setAppName("RDD")
+
+    val conf = new SparkConf().setMaster("local[8]").setAppName("RDD")
     val sc = new SparkContext(conf)
     /**
      *
      *     内存加载分区规则
      *       case _ =>
-                val array = seq.toArray // To prevent O(n^2) operations for List etc
-                positions(array.length, numSlices).map { case (start, end) =>
-                    array.slice(start, end).toSeq
-                }.toSeq
+     *           val array = seq.toArray // To prevent O(n^2) operations for List etc
+     *           positions(array.length, numSlices).map { case (start, end) =>
+     *               array.slice(start, end).toSeq
+     *           }.toSeq
      *     def positions(length: Long, numSlices: Int): Iterator[(Int, Int)] = {
-                  (0 until numSlices).iterator.map { i =>
-                    val start = ((i * length) / numSlices).toInt
-                    val end = (((i + 1) * length) / numSlices).toInt
-                    (start, end)
-                  }
-                }
+     *             (0 until numSlices).iterator.map { i =>
+     *               val start = ((i * length) / numSlices).toInt
+     *               val end = (((i + 1) * length) / numSlices).toInt
+     *               (start, end)
+     *             }
+     *       }
      */
 
     val rdd = sc.makeRDD(List(1, 2, 3, 4),3)
